46459cb85fee14894319b5c074a65563ec13442d
Martins Ngene <martinsngene9@gmail.com>
2024-12-06 12:52:35 +0100

    update: minor edit

--- components/send-message.tsx
+++ components/send-message.tsx
@@ -47,7 +47,7 @@ const SendMessage = ({ user }: any) => {
         type="text"
         value={newMessage}
         onChange={(e) => handleInput(e)}
-        placeholder="Jane Smith"
+        placeholder="Enter a message"
         className="block w-[80%] rounded bg-white px-4 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
       />
       <button
